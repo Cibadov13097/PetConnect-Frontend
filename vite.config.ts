83755f41db2fbab4d8d742458910f3db0b16cc77
CavidIbadov <cibadov13097@ada.edu.az>
2025-09-16 20:43:37 +0400

    prepare for deploy

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,8 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-    base: mode === "production" ? "/critter-connections/" : "/", 
+   base: mode === "production" ? "/PetConnect-Frontend/" : "/"
+,
   server: {
     host: "::",
     port: 8080,
